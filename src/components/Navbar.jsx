806d6289cd03c0f0f31f7adb2b596154abc3c130
Victor Ondari <99796872+Vonmak@users.noreply.github.com>
2023-02-13 11:39:52 +0300

    minor updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/vic.png";
 import { Theme } from "./theme/Theme";
 
@@ -13,7 +13,7 @@ const navigation = [
   },
 ];
 
-export default function Navbar() {
+const Navbar = () => {
   const [navbarClass, setNavbarClass] = useState("");
   const [textClass, setTextClass] = useState("");
 
@@ -28,34 +28,39 @@ export default function Navbar() {
       }
     };
     window.addEventListener("scroll", handleScroll);
+
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
   return (
-    <div
-      className={`flex items-center justify-between  px-24 fixed top-0 w-full z-10  ${navbarClass} bg-transparent`}
+    <nav
+      className={`flex items-center justify-between px-24 fixed top-0 w-full z-10 ${navbarClass}`}
       aria-label="Global"
     >
-      <div className="flex items-center ">
+      <div className="flex items-center">
         <Link to="/" className="mr-6">
-          <img src={logo} alt="" className="rounded-full w-16 h-16 pt-1" />
+          <img src={logo} alt="logo" className="rounded-full w-16 h-16 pt-1" />
         </Link>
       </div>
-      <div className="hidden lg:flex lg:gap-x-12">
+      <ul className="hidden lg:flex lg:gap-x-12">
         {navigation.map((item, index) => (
-          <Link
-            key={item.name + index}
-            to={item.to}
-            className={`text-lg font-bold leading-6 ${textClass}  cursor-pointer`}
-          >
-            {item.name}
-          </Link>
+          <li key={item.name + index}>
+            <Link
+              to={item.to}
+              className={`text-lg font-bold leading-6 cursor-pointer ${textClass}`}
+            >
+              {item.name}
+            </Link>
+          </li>
         ))}
-      </div>
-      <div className="flex items-center">
+      </ul>
+      <div className={textClass}>
         <Theme />
       </div>
-    </div>
+    </nav>
   );
-}
+};
+
+export default Navbar;
