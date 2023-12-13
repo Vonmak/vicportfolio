a7489f616c7b5f515e52597e71eaf8eae2e04c74
Guest <von.makori@gmail.com>
2023-12-13 19:32:44 +0300

    updated the navbar section.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,60 +12,58 @@ const navigation = [
   { name: "Experience", to: "/experience" },
   {
     name: "Download my CV",
-    to: "https://drive.google.com/file/d/1Lcx1-iZwV_bGCChPxl6CA2qDaphX-l3Y/view?usp=share_link",
+    to:
+      "https://drive.google.com/file/d/1Lcx1-iZwV_bGCChPxl6CA2qDaphX-l3Y/view?usp=share_link",
   },
 ];
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
-  const [navbarClass, setNavbarClass] = useState("");
-  const [textClass, setTextClass] = useState("");
+  const [scrollPosition, setScrollPosition] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setNavbarClass("bg-gray-800");
-        setTextClass("text-white");
-      } else {
-        setNavbarClass("");
-        setTextClass("");
-      }
+      setScrollPosition(window.scrollY);
     };
-    window.addEventListener("scroll", handleScroll);
 
+    window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
-  const myName = "{ Victor }";
+  const myName = "Victor";
 
   return (
     <nav
-      className={`flex flex-row justify-between py-5 items-center px-2 md:px-5 z-50 fixed top-0 w-full ${navbarClass}`}
+      className={`flex justify-between py-5 items-center px-2 md:px-5 z-50 fixed top-0 w-full shadow-md ${
+        scrollPosition > 50 ? "bg-gray-800 text-white" : ""
+      }`}
     >
-      <Link to="/" className="flex items-center ">
+      <Link to="/" className="flex items-center">
         <img
           src={logo}
           alt="logo"
-          className=" mr-6 rounded-full w-16 h-16 pt-1"
+          className="mr-6 rounded-full w-16 h-16 pt-1"
         />
-        <code className="font-semibold text-2xl text-teal-400">{myName}</code>
+        <code className="font-semibold text-2xl text-teal-400">{`{ ${myName} }`}</code>
       </Link>
 
       <div
-        className={`flex flex-col absolute bg-black opacity-[95%]  top-0 duration-300  ${textClass} ${
-          active ? `-right-[0%]` : `-right-[100%]`
-        }  h-screen w-full items-start pl-[25%] justify-center space-y-5 md:right-0 md:items-center md:pl-0 md:opacity-[100%] md:h-max md:space-y-0 md:bg-transparent  md:space-x-5 md:relative md:flex-row md:top-0 md:w-max`}
+        className={`flex flex-col absolute top-0 py-5 duration-300 ${
+          scrollPosition > 50 ? "bg-gray-800 text-white" : "bg-slate-300 text-gray-800"
+        }  ${
+          active ? "-right-[0%]" : "-right-[100%]"
+        } w-full items-center justify-center space-y-5 md:right-0 md:items-center md:opacity-[100%] md:h-max md:space-y-0 md:bg-transparent md:space-x-5 md:relative md:flex-row md:top-0 md:w-max`}
       >
-        <div className="block absolute text-2xl font-black top-10 right-10 md:hidden">
+        <div className="block text-2xl md:hidden">
           <RxCross1 onClick={() => setActive(!active)} />
         </div>
         {navigation.map((item, index) => (
           <Link
             to={item.to}
             key={item.name + index}
-            className={`text-xl font-extrabold md:text-xl ${textClass}`}
+            className={`text-xl font-extrabold md:text-xl`}
             onClick={() => setActive(!active)}
           >
             {item.name}
@@ -76,12 +74,13 @@ const Navbar = () => {
           <Theme />
         </div>
       </div>
+
       <div className="flex flex-row items-center space-x-3 md:hidden">
         <div className="block md:hidden">
           <Theme />
         </div>
         <AiOutlineMenu
-          className={`text-2xl ${textClass}`}
+          className="text-2xl"
           onClick={() => setActive(!active)}
         />
       </div>
